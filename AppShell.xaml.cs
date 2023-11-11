24b2e616fbb92298e616a79a37535e3450c227c6
webtechcompany <lincon.santos@contact-webtechcompany.com.br>
2023-11-11 08:29:28 -0300

    feat: Initial Commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RigelVoyager;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RigelVoyager;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
